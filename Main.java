2b5d9ecc3dba710315c7d29197b25e3f31417c76
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-01-28 17:02:28 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+         String surname = "Valenti";
+         System.out.println("Il mio cognome " + (surname.contains("ni") ? "contiene " : "non contiene") + " la sequenza di lettere ni");
+
+    }
+}
